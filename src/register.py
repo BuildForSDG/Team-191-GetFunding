6971a0b08fb6090382698e9881b5ee0a4b6d35de
CalvoM <mwadimemakokha@ieee.org>
2020-05-09 18:26:04 +0300

    feat: User registration
    Add error handling for missing keys in incoming data from client

--- src/register.py
+++ src/register.py
@@ -13,16 +13,23 @@ def reg_lender():
     user_data = request.get_json()
     if user_data is None:
         abort(400)
-    if is_user_in_db(user_data):
+    err = is_user_in_db_err(user_data)
+    if err is LookupError:
         return Response("User in db", 409)
-    name = user_data["name"]
-    email = user_data["email"]
-    password = user_data["password"]
-    phone_number = user_data["phone_number"]
-    lender = Lender()
-    user = User(name=name, email=email, password=generate_password_hash(password=password), phone_number=phone_number, lender=lender)
-    db.session.add(user)
-    db.session.commit()
+    elif err is KeyError:
+        return Response(err)
+    try:
+        name = user_data["name"]
+        email = user_data["email"]
+        password = user_data["password"]
+        phone_number = user_data["phone_number"]
+        lender = Lender()
+        user = User(name=name, email=email, password=generate_password_hash(password=password), phone_number=phone_number, lender=lender)
+        db.session.add(user)
+        db.session.commit()
+    except KeyError as key:
+        return Response("Check "+str(key),400)
+    
     return Response("Lender has been added!", 200)
 
 
@@ -34,28 +41,41 @@ def reg_borrower():
     user_data = request.get_json()
     if user_data is None:
         abort(400)
-    if is_user_in_db(user_data):
+    if user_data is None:
+        abort(400)
+    err = is_user_in_db_err(user_data)
+    if err is LookupError:
         return Response("User in db", 409)
-    name = user_data["name"]
-    email = user_data["email"]
-    password = user_data["password"]
-    phone_number = user_data["phone_number"]
-    borrower = Borrower()
-    user = User(name=name, email=email, password=generate_password_hash(password=password), phone_number=phone_number, borrower=borrower)
-    db.session.add(user)
-    db.session.commit()
+    elif err is KeyError:
+        return Response(err)
+    try:
+        name = user_data["name"]
+        email = user_data["email"]
+        password = user_data["password"]
+        phone_number = user_data["phone_number"]
+        borrower = Borrower()
+        user = User(name=name, email=email, password=generate_password_hash(password=password), phone_number=phone_number, borrower=borrower)
+        db.session.add(user)
+        db.session.commit()
+    except KeyError as key:
+        return Response("Check "+str(key),400)
     return Response("Borrower has been added!", 200)
 
 
-def is_user_in_db(user_info):
+def is_user_in_db_err(user_info):
     """
     Check if user in the database
     """
-    print(user_info)
-    email = user_info["email"]
-    email_result = User.query.filter_by(email=email).first()
-    phone = user_info["phone_number"]
-    phone_result = User.query.filter_by(phone_number=phone).first()
+    email_result = None
+    phone_result = None
+    err = None
+    try:
+        email = user_info["email"]
+        email_result = User.query.filter_by(email=email).first()
+        phone = user_info["phone_number"]
+        phone_result = User.query.filter_by(phone_number=phone).first()
+    except KeyError as key:
+        return key
     if email_result is not None or phone_result is not None:
-        return True
-    return False
+        return LookupError
+    return None
