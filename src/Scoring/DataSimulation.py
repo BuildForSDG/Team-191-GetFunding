a502f4ec344bd0cd7a093850b2bbeda40c190e5c
sikolia <sikolia.wycliffe@gmail.com>
2020-05-13 13:08:04 +0300

    feat: generate transactional and loan datasets

--- src/Scoring/DataSimulation.py
+++ src/Scoring/DataSimulation.py
@@ -7,12 +7,7 @@ import random
 import pandas as pd
 import numpy as np
 import os
-import pickle
-import time
-import datetime
-from datetime import date, timedelta
-
-
+from datetime import date
 
 
 start_date = date(2020, 1, 1)
@@ -20,82 +15,101 @@ end_date = date(2020, 4, 1)
 # check is used in the create_dataset function to generate date column
 check = pd.date_range(start_date, end_date).to_list()
 
-def create_trans_dataset(num=1):
-    """use faker to generate a synthetic dataset. The amount
-    is generated using a different function. The different
-    function is below this function and is the immediate
-    function below this function"""
-    output=[
-            {"transaction_id": list(range(1, num)),
-             "borrower_id": np.random.choice(list(range(1, 5000)),
-                                              size=num, replace=True),
-             "trans_date": np.random.choice(check),
-             "trans_details":np.random.choice(["airtime_purchase",
-                                               "customer_transfer",
-                                               "deposit_funds",
-                                               "withdrawal_amount"],
-                                              p=[0.2, 0.2, 0.2, 0.1, 0.1, 0.2]),
-             "trans_status": np.random.choice(["completed", "reversed",
-                                               "procesing"],
-                                               p=[0.9, 0.05, 0.05])
-           }
-            for x in range(num)
-          ]
+
+def create_dataset(num=1):
+    """Generate a synthetic dataset.
+    Given an integer of the number of rows expected in the data set
+    Generate a trasaction date column, transaction details column, and
+    the transaction status columns for the transaction dataset."""
+    output = [
+              {"trans_date": np.random.choice(check),
+               "trans_details": np.random.choice(["airtime_purchase",
+                                                  "customer_transfer",
+                                                  "deposit_funds",
+                                                  "withdrawal"],
+                                                 p=[0.4, 0.2, 0.2, 0.2]),
+               "trans_status": np.random.choice(["completed", "reversed",
+                                                 "procesing"],
+                                                p=[0.9, 0.05, 0.05])
+               }
+              for x in range(num)
+            ]
     return output
 
-trans_dataset = pd.DataFrame(create_dataset(num=20000))
+
+trans_dataset = pd.DataFrame(create_dataset(num=60000))
 
 
-def create_trans_amount(df):
-    """Generates the amount colemn in the trans_dataset.
-    generate different amounts given the trans_details in the
-    trans_dataset. For example if the trans_details is
-    airtime_purchase it generates a random number between 5
-    and five thousand. It then adds the trans_amount column
-    to the trans_dataset."""
-
-    df['amount'] = np.where(df.trans_details == "airtime_purchase",
-                            random.randint(5, 5000),
-                            np.where(df.trans_details == "customer_transfer",
-                            random.randint(100, 35000),
-                            np.where(df.trans_details == "deposit_funds",
-                            random.randint(100, 35000),
-                            np.where(df.trans_details == "withdrawal_amount",
-                            random.randint(100, 35000),
-                            np.where(df.trans_details == "withdrawal_charge",
-                            random.randint(33, 1000), random.randint(33, 1000)
-                            )))))
-    df["transaction_id"] = list(range(1, 20001))
+def create_trans_ids(df):
+    """Given a data frame generate transaction id which is unique for
+     every row and a borrower id which is not unique to every row.
+     Add the ids to the data frame and return the data frame"""
+    df["transaction_id"] = list(range(1, 60001))
     df["borrower_id"] = np.random.choice(list(range(1, 5000)),
-                                               size=20000, replace=True)
+                                         size=60000, replace=True)
     return df
 
-trans_dataset = create_trans_amount(trans_dataset)
 
+trans_dataset = create_trans_ids(trans_dataset)
+
+
+def create_sub_dataset(df, containing, column_filter, myrange):
+    """Given a dataframe, filter the dataframe for rows containing
+    'containing', on column 'column_filter' and generate a list of
+    random numbers as the amount and add the amount as a column
+    to the data frame. Add a column of charge as 3 percent of the
+    amount and return a dataframe."""
+    df = df[df[str(column_filter)].str.contains(str(containing))]
+    df["amount"] = np.random.choice(list(myrange),
+                                    size=len(df["trans_status"]),
+                                    replace=True)
+    if not containing == "airtime":
+        df["charge"] = df["amount"] * 0.03
+    return df
+
+
+withdrawals = create_sub_dataset(trans_dataset, "withdrawal",
+                                 "trans_details", range(100, 30000))
+customer_transfers = create_sub_dataset(trans_dataset, "transfer",
+                                        "trans_details", range(100, 35000))
+deposits = create_sub_dataset(trans_dataset, "deposit",
+                              "trans_details", range(100, 70000))
+airtime_purchases = create_sub_dataset(trans_dataset, "airtime",
+                                       "trans_details", range(100, 5000))
 
 
 def create_loan_dataset(num=1):
-    """ The amount is generated using a different function.
-    The different function is below this function and is the immediate
-     function below this function"""
-    output=[
-            {
-             "repayment_period": np.random.choice([1, 2, 3],
-                                                  p=[0.7, 0.2, 0.1]),
-             "date_disbursed":np.random.choice(check),
-             "loan_amount":random.randrange(100,35000,5)
-           }
-            for x in range(num)
-          ]
+    """ Generate a loan data set.
+    Generate a repayment period a date disbursed and a loan amount.
+    """
+    output = [
+              {"repayment_period": np.random.choice([1, 2, 3],
+                                                    p=[0.7, 0.2, 0.1]),
+               "date_disbursed": np.random.choice(check),
+               "loan_amount": random.randrange(100, 35000, 5)
+               }
+              for x in range(num)
+             ]
     return output
 
+
+loan_dataset = pd.DataFrame(create_loan_dataset(num=20000))
+
+
 def add_loan_columns(df, num):
+    """Generate additional columns for the loan dataset.
+    Given a dataframe generate a date repaid column as the
+    date_disbursed plus the repayment_period. Add a loan_id
+    of unique loans and a borrower id which is not unique."""
     df["date_repaid"] = np.where(df.repayment_period == 1,
-                            df["date_disbursed"]+ pd.offsets.MonthOffset(1),
-                            np.where(df.repayment_period == 2,
-                            df["date_disbursed"]+ pd.offsets.MonthOffset(2),
-                            df["date_disbursed"]+ pd.offsets.MonthOffset(3)))
-    df["loan_id"] =  list(range(1, num+1))
+                                 df["date_disbursed"] +
+                                 pd.offsets.MonthOffset(1),
+                                 np.where(df.repayment_period == 2,
+                                          df["date_disbursed"] +
+                                          pd.offsets.MonthOffset(2),
+                                          df["date_disbursed"] +
+                                          pd.offsets.MonthOffset(3)))
+    df["loan_id"] = list(range(1, len(df["date_repaid"]) + 1))
     df["borrower_id"] = np.random.choice(list(range(1, 5000)),
-                                                  size=num, replace=True)
+                                         size=num, replace=True)
     return df
