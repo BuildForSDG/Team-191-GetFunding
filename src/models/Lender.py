e0be6ad671f375a20879ab77c8cf3856b8ccaab1
CalvoM <mwadimemakokha@ieee.org>
2020-05-16 03:24:31 +0300

    Observe pep8 recommendations

--- src/models/Lender.py
+++ src/models/Lender.py
@@ -3,8 +3,6 @@ import src
 
 class Lender(src.db.Model):
 
-    """Lender model."""
-
     id = src.db.Column(src.db.Integer, primary_key=True)
     amount = src.db.Column(src.db.Integer, nullable=False, default=0)
     limit = src.db.Column(src.db.Integer, default=0)
