f2abb0252e440827a73af8489dd7d0d9b211b45c
sikolia <sikolia.wycliffe@gmail.com>
2020-05-25 12:02:26 +0300

    refactor: rename config.py to configuration.py

---
+++ configuration.py
@@ -0,0 +1,28 @@
+"""configure the application using a python module called
+decouple."""
+
+
+from decouple import config
+
+
+
+class MainConfig():
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = config('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(MainConfig):
+    DB_NAME = 'ProductionCreditScore'
+    SQLALCHEMY_DATABASE_URI = config('PRODUCTION_URI')
+
+
+class DevelopmentConfig(MainConfig):
+    DEBUG = True
+    DB_NAME = 'DevelopmentCreditScore'
+    SQLALCHEMY_DATABASE_URI = config('DEVELOPMENT_URI')
+
+class TestingConfig(MainConfig):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = config('TESTING_URI')
