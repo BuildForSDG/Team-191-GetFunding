e7542c960b358e873180d6fda1eeb11ff4b879d1
Kefa Mutuma <mutmakeffa@gmail.com>
2020-05-26 12:28:52 +0300

    fixes: changed all single quotes to double quotes to be able to pass the tests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
-import Header from './components/layout/Header';
-import Landing from './components/Landing';
-import Login from './components/Auth/Login';
-import RegisterLender from './components/Auth/Register/RegisterLender';
-import RegisterBorrower from './components/Auth/Register/RegisterBorrower';
+import Header from "./components/layout/Header";
+import Landing from "./components/Landing";
+import Login from "./components/Auth/Login";
+import RegisterLender from "./components/Auth/Register/RegisterLender";
+import RegisterBorrower from "./components/Auth/Register/RegisterBorrower";
 
 export default function App() {
   return (
@@ -17,16 +17,16 @@ export default function App() {
         <Header />
         <div className="container">
           <Switch>
-            <Route exact path='/' component={Landing} />
+            <Route exact path="/" component={Landing} />
           </Switch>
           <Switch>
-            <Route exact path='/borrower' component={RegisterBorrower} />
+            <Route exact path="/borrower" component={RegisterBorrower} />
           </Switch>
           <Switch>
-            <Route exact path='/lender' component={RegisterLender} />
+            <Route exact path="/lender" component={RegisterLender} />
           </Switch>
           <Switch>
-            <Route exact path='/login' component={Login} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </div>
       </>
--- frontend/src/components/Auth/Register/RegisterLender.js
+++ frontend/src/components/Auth/Register/RegisterLender.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import {Link} from "react-router-dom";
 
 export default function RegisterLender() {
     
