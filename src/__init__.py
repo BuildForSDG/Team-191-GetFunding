ae4d20af8be5d8a87ce4399a06ca1386085d4a81
CalvoM <mwadimemakokha@ieee.org>
2020-05-29 17:26:38 +0300

    Improve configuration handling

--- src/__init__.py
+++ src/__init__.py
@@ -5,11 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 db = SQLAlchemy()
 from src import models
-
+from decouple import config
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(os.environ['SDG_CONFIG'])
+    app.config.from_object(config('SDG_CONFIG'))
     with app.app_context():
         db.init_app(app)
         from src.Users import register_bp
